0a97a7711bba9ddb6cc3c17121ef9467b8cad133
Son Ho - May cong ty <quangson@hasakeplay.com.vn>
2018-08-30 11:20:15 +0700

    upload file bai hoc so 2

---
+++ python/codeif.py
@@ -0,0 +1,10 @@
+print('tên là gì em?')
+name = input()
+print('bao tuổi?')
+age = int(input())
+if name == 'Mary':
+    print('chuẩn mẹ nó tên là ' + name + ' rồi')
+elif age >= 12:
+    print('Già quá rồi')
+elif age <=12:
+    print ('Cũng được đây')
