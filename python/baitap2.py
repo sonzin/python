fd126d540da27680fd3d7a948f3bb376f3347f07
Ho Quang Son <hqson1304@hotmail.com>
2018-09-01 21:43:16 +0700

    Cập nhật thêm thông báo trả lại tiền cho khách

--- python/baitap2.py
+++ python/baitap2.py
@@ -2,3 +2,9 @@ price = input('Nhập giá sản phẩm vào đây em: ')
 paid=input('Nhập vào đây số tiền khách trả: ')
 change = float(paid)-float(price)
 print('Trả lại khách hàng số tiền là: ', change)
+if (change < 0):
+    print('Khách trả thiếu tiền rồi em')
+elif (change >500):
+    print('Anh trả thừa nhiều quá, em ko có tiền trả lại')
+elif(change <=500 and change >=100):
+    print('Chờ tí em trả lại cho')
