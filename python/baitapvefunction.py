4cae462505fe306c909744eb1762588fe5e146cc
Ho Quang Son <hqson1304@hotmail.com>
2018-09-01 22:06:15 +0700

    Gọi hàm trong hàm

--- python/baitapvefunction.py
+++ python/baitapvefunction.py
@@ -1,7 +1,13 @@
+def daungancach():
+    print('--------------------------')#tạo dấu ngăn cách giữa các hàm
+
 def danhsachmuahang():
     print('Trà sữa')
     print('Dilmah')
     print('Sữa chua')
     print('Sữa tươi')
-    print()
+    daungancach()
+
 danhsachmuahang()
+danhsachmuahang()
+
