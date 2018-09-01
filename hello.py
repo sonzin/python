b5a2c66aea2607f725ada6e4fd45be7ba6732038
Ho Quang Son <hqson1304@hotmail.com>
2018-09-01 21:34:58 +0700

    Cập nhật bài tập 2

---
+++ hello.py
@@ -0,0 +1,15 @@
+#day la chuong trinh dau tien cua toi tren python
+
+print('Xin chao the gioi')
+print('Ban ten la gi?') #hoi ten 1 nguoi ban
+myname = input()
+print('it is good to see you,' + myname)
+print('do dai cua ten ban la:')
+print(len(myname))
+print('Ban bao nhieu tuoi roi') #hoi tuoi ban be
+myage = input()
+print('vay la ban da ', myage, 'tuoi roi')
+print('ban se ' + str(int(myage)+1) + ' tuoi trong vong 1 nam nua')
+print('Mày đang học lớp nào hả em?')
+myclass=input()
+print('Tao đang học lớp ' , myclass, ' con ạ') 
