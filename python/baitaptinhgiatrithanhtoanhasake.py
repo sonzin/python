7fd69ae8a7926cd8e687f2161b3dd20ca13c60d1
Ho Quang Son <hqson1304@hotmail.com>
2018-09-02 22:47:16 +0700

    Cập nhật thêm quy đổi tỉ giá

--- python/baitaptinhgiatrithanhtoanhasake.py
+++ python/baitaptinhgiatrithanhtoanhasake.py
@@ -2,12 +2,15 @@ total= int(input('Tổng giá trị hợp đồng: '))
 payment1 = int(input('Thanh toán lần 1: '))
 payment2 = int(input('Thanh toán lần 2: '))
 payment3 = int(input('Thanh toán lần 3: '))
+rate = int(input('Nhập tỉ giá USD/VND: '))
 checktrue= payment1+payment2+payment3
 def tinhtoangiatricaclan(total):     
-    print('Thanh toán lần 1 sẽ là: ',total*int(payment1)/100)
-    print('Thanh toán lần 2 sẽ là: ',total*int(payment2)/100)
-    print('Thanh toán lần 3 sẽ là: ',total*int(payment3)/100)
+    print('Thanh toán lần 1 sẽ là: ',total*int(payment1)/100,'. Quy đổi VNĐ:',total*int(payment1)/100*rate)
+    print('Thanh toán lần 2 sẽ là: ',total*int(payment2)/100,'. Quy đổi VNĐ:',total*int(payment2)/100*rate)
+    print('Thanh toán lần 3 sẽ là: ',total*int(payment3)/100,'. Quy đổi VNĐ:',total*int(payment3)/100*rate)
     
 if checktrue == 100:
     tinhtoangiatricaclan(total)
 else: print('Vui lòng check lại số liệu phần trăm')
+
+
