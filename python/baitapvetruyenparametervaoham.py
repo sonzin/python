24164f594eec5e9f21ae8a1b3f6b5cc6be3390c0
Ho Quang Son <hqson1304@hotmail.com>
2018-09-02 22:21:13 +0700

    Bài tập về truyền giá trị vào Parameter của Hàm

---
+++ python/baitapvetruyenparametervaoham.py
@@ -0,0 +1,7 @@
+def hienthidanhsach(tendanhsach):
+    print('Dưới đây là danh sách số ',tendanhsach)
+    print('táo')
+    print('lê')
+    print('đào')
+tendanhsach=input('nhập vào đi em: ')
+hienthidanhsach(tendanhsach)
